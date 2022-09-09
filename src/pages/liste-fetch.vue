5a9718f94909400ff592b40b9fee2b487c81ebb4
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-09 19:12:42 +0200

    Ajout du contenu de la page liste fetch

---
+++ src/pages/liste-fetch.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AfficheMaison from "../components/AfficheMaison.vue";
+const res = await fetch("maisons.json");
+const dataMaisons = await res.json();
+</script>
+
+<template>
+    <div class="p-5">
+        <h1>Page Liste fetch</h1>
+        <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(343px,1fr))]">
+            <div class="p-4" v-for="dataMaison in dataMaisons" :key="dataMaison.nom">
+                <AfficheMaison v-bind="dataMaison" />
+            </div>
+        </div>
+    </div>
+</template>
+
