fae735ff1ee14078425c010b66303ad1d78459c8
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-09 13:37:03 +0200

    Création de la page simple.vue + ajout icones

---
+++ src/pages/simple.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    import Card from "../components/card.vue"
+</script>
+
+<template>
+    <div class="p-2">
+        <h2>Page Simple</h2>
+        <Card />
+    </div>
+</template>
