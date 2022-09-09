21730dba37c4365a13e951df4679fbff5840c817
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-09 14:46:53 +0200

    Ajout d'icones

---
+++ src/components/icones/FlecheBas.vue
@@ -0,0 +1,20 @@
+<template>
+    <svg
+  width="20"
+  height="20"
+  viewBox="0 0 20 20"
+  fill="none"
+  xmlns="http://www.w3.org/2000/svg"
+  class="flex-grow-0 flex-shrink-0 w-5 h-5"
+  preserveAspectRatio="xMidYMid meet"
+>
+  <circle cx="10" cy="10" r="10" fill="#E0E7FF"></circle>
+  <path
+    d="M6.5 8.25L10 11.75L13.5 8.25"
+    stroke="#6366F1"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  ></path>
+</svg>
+</template>
