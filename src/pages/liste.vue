a5ade4946f86e621433030654d6cfdaaaef81944
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-09 15:35:27 +0200

    Début aide liste

--- src/pages/liste.vue
+++ src/pages/liste.vue
@@ -1,3 +1,24 @@
 <script setup lang="ts">
-    const maisons = [ {/* Obj 1re maison */}, { /* Obj 2nd */ }, /* ... */ ];
+import AfficheMaison from "../components/AfficheMaison.vue"
+    const dataMaisons = [{
+        titre:"Palm Harbor",
+         price:2095, 
+         favoris:true,
+          lieu:"2699 Green Valley, Highland Lake ", 
+          nbrSDB:2, 
+          image:"/images-maison/maison2.jpg"
+        
+    }];
 </script>
+
+<template>
+    <div class="p-2">
+        <h2>Page Simple</h2>
+        <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(343px,1fr))]">
+            <div v-for="dataMaison in dataMaisons" :key="dataMaison.price"><AfficheMaison  v-bind="dataMaison"/></div>
+            
+           
+            
+        </div>
+    </div>
+</template>
