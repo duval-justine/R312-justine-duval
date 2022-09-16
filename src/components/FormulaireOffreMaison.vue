bdb238a87c36f4116054ac4f85abe8f637cfeaf4
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-16 15:07:58 +0200

    Modif apparence du form

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -11,12 +11,21 @@ const dataMaison = ref({});
             <AfficheMaison v-bind="dataMaison" />
         </div>
         <div class="p-2">
-            <FormKit type="form" v-model="dataMaison">
-                <FormKit name="titre" label="Nom de la maison" />
-                <FormKit name="price" label="Prix de la maison" type="number" />
-                <FormKit name="favoris" label="Favoris" type="checkbox" />
+            <FormKit type="form" :submit-attrs="{ classes: {input: 'bg-indigo-100 hover:bg-indigo-200 rounded p-2'}}"
+                :config="{ classes: {input: 'border-gray-300 p-1 rounded shadow-sm border'}}" v-model="dataMaison">
+                <div class="mb-2">
+                    <FormKit name="titre" label="Nom de la maison" />
+                </div>
+                <div class="mb-2">
+                    <FormKit name="price" label="Prix de la maison" type="number" />
+                </div>
+                <div class="mb-2">
+                    <FormKit name="favoris" label="Ajouter aux favoris" type="checkbox" wrapper-class="flex" />
+                </div>
                 <FormKit name="lieu" label="Adresse de la maison" />
-                <FormKit name="nbrSDB" label="Nombre de salle de bain" type="number" />
+                <div class="mb-8">
+                    <FormKit name="nbrSDB" label="Nombre de salle de bain" type="number" />
+                </div>
             </FormKit>
         </div>
     </div>
