a6b0bdb5596db84120c6118dba91eb0dea41b433
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-16 14:00:43 +0200

    Création du composents du Formulaire de Contact

---
+++ src/components/FormulaireOffreMaison.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "@vue/reactivity";
+import AfficheMaison from "./AfficheMaison.vue";
+const dataMaison = ref({});
+</script>
+
+<template>
+    <div>
+        <div class="p-2">
+            <h2 class="text-2xl">Résultat (Prévisualisation)</h2>
+            <AfficheMaison v-bind="dataMaison" />
+        </div>
+        <div class="p-2">
+            <FormKit type="form" v-model="dataMaison">
+                <FormKit name="titre" label="nom" />
+                <FormKit name="price" label="prix" type="number" />
+                <FormKit name="favoris" label="mettre en valeur" type="checkbox" />
+                <FormKit name="lieu" label="name" />
+                <FormKit name="nombre de salle de bain" label="number" type="number" />
+            </FormKit>
+        </div>
+    </div>
+</template>
