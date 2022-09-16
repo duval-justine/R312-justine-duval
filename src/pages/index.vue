3cac42aa1276d7b8212a0ed0eee065fe1e1ba9fb
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-16 15:17:24 +0200

    Changement du message lors de l'arrivé sur le site

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <section>
-    <h1 class="text-2xl">
-      Bonjour<code class="font-mono text-base">/src/pages/index.vue</code>
-    </h1>
-  </section>
+  <main class="bg-indigo-25 pt-14 px-4 font-inter">
+    <h2 class="text-4xl font-bold text-indigo-1000 text-center pt-[3.625rem] mb-8">Bienvenue sur Estatery Agency !
+    </h2>
+  </main>
 </template>
