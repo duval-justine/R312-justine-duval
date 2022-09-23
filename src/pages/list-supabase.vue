3db3767b59e8a1ec4637f2744c73c74d05be9015
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-23 15:19:36 +0200

    Ajout du composant dans la liste supabase

--- src/pages/list-supabase.vue
+++ src/pages/list-supabase.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import AfficheMaison from "../components/AfficheMaison.vue";
+
 import { supabase } from "../supabase";
 console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
 // const maisons = []; // à remplacer par l'appel à Supabase
@@ -14,7 +16,7 @@ console.log(data)
         <h2 class="text-3xl font-bold text-indigo-1000 text-center pt-[3.625rem] mb-8">Liste des maisons de Supabase
         </h2>
         <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(343px,1fr))]">
-            <div class="p-4" v-for="data in dataMaisons" :key="dataMaison.nom">
+            <div class="p-4" v-for="dataMaison in data" :key="dataMaison.nom">
                 <AfficheMaison v-bind="dataMaison" />
             </div>
         </div>
