3840d0b2fe0137dc92a4ae16eb259f75b5129670
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-23 15:06:13 +0200

    Ajout de propriété dans le composants

--- src/components/AfficheMaison.vue
+++ src/components/AfficheMaison.vue
@@ -4,45 +4,54 @@ import bath from "./icones/Bath.vue"
 import dimension from "./icones/Dimension.vue"
 import heart from "./icones/Heart.vue"
 
-defineProps  ({
-        price: {
-            type: Number,
-            default: 2700,
-        },
-        favoris : {
-            type: Boolean,
-            default: false, 
-        },
-        titre: {
-            type: String,
-            required:true
-        },
-        image: String,
-        nbrSDB: Number,
-        lieu: String,
+defineProps({
+    price: {
+        type: Number,
+        default: 2700,
     },
-  );
+    favoris: {
+        type: Boolean,
+        default: false,
+    },
+    titre: {
+        type: String,
+        required: true
+    },
+    image: String,
+    surface: String,
+    nbrSDB: Number,
+    nbrChambres: Number,
+    lieu: String,
+},
+);
 </script>
 
 <template>
-  <figure>
-      <img class="rounded-t-lg" :src="image" alt="">
-      <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
-          <h3 class="text-2xl text-gray-900 font-bold">{{titre}}</h3>
-          <address class="not-italic text-gray-500">{{ lieu }}</address>
-          <div class="order-first flex items-center">
-              <div class="text-2xl text-indigo-500 font-bold">${{ price.toLocaleString("en-US") }}</div>
-              <div class="text-gray-500 ">/month</div>
-          </div>
-          <hr class="border-indigo-100 border-t-2 my-2"/>
-          <div class="flex justify-between text-sm">
-              <div><bed class="inline-block align-top"/>4 Beds </div>
-              <div><bath class="inline-block align-top"/> {{ nbrSDB }} Bathrooms </div>
-              <div><dimension class="inline-block align-top"/> 6x7.5 m² </div>
-          </div>
-          <div class="grid place-items-center w-12 h-12 border-2 border-indigo-100 rounded-full absolute top-7 right-5">
-              <heart :class="{'fill-indigo-500': favoris}"/>
-          </div>
-      </figcaption>
-  </figure>
+    <figure>
+        <img class="rounded-t-lg" :src="image" alt="">
+        <figcaption class="flex flex-col relative px-5 py-7 border-2 border-t-0 rounded-b-lg">
+            <h3 class="text-2xl text-gray-900 font-bold">{{titre}}</h3>
+            <address class="not-italic text-gray-500">{{ lieu }}</address>
+            <div class="order-first flex items-center">
+                <div class="text-2xl text-indigo-500 font-bold">${{ price.toLocaleString("en-US") }}</div>
+                <div class="text-gray-500 ">/month</div>
+            </div>
+            <hr class="border-indigo-100 border-t-2 my-2" />
+            <div class="flex justify-between text-sm">
+                <div>
+                    <bed class="inline-block align-top" /> {{nbrChambres}} Beds
+                </div>
+                <div>
+                    <bath class="inline-block align-top" /> {{ nbrSDB }} Bathrooms
+                </div>
+                <div>
+                    <dimension class="inline-block align-top" /> {{surface}} m²
+                </div>
+            </div>
+            <div
+                class="grid place-items-center w-12 h-12 border-2 border-indigo-100 rounded-full absolute top-7 right-5">
+                <heart :class="{'fill-indigo-500': favoris}" />
+            </div>
+        </figcaption>
+    </figure>
 </template>
