5b441ba265b02ca29f1667329bb9d86542ddd56b
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-09-30 15:00:43 +0200

    Mofid des boutons du login

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -13,13 +13,25 @@ async function signIn(data, node) {
 const nvlUtilisateur = ref(false);
 </script>
 <template>
-    <div>
-        <button v-if="user" @pointerdown="supabase.auth.signOut()">
+    <div class="flex justify-center">
+        <button class="mt-4 bg-violet-300 border-violet-500 hover:bg-violet-500 rounded p-2" v-if="user"
+            @pointerdown="supabase.auth.signOut()">
             Se déconnecter ({{ user.email }})
         </button>
-        <FormKit v-else type="form" :submit-label="nvlUtilisateur ? 'S inscrire' : 'Se connecter'" @submit="signIn">
-            <FormKit name="email" label="Votre eMail" type="email" />
-            <FormKit name="password" label="Mot de passe" type="password" />
+        <FormKit v-else type="form"
+            :submit-attrs="{ classes: {input: 'mt-4 bg-violet-300 border-violet-500 hover:bg-violet-500 rounded p-2'}}"
+            :submit-label="nvlUtilisateur ? 'S inscrire' : 'Se connecter'" @submit="signIn">
+            <div class="mt-32 mb-8">
+                <FormKit name="email" :config="{ classes: {input: 'border-gray-300 p-1 rounded shadow-sm border'}}"
+                    label="Votre adresse mail" type="email" />
+            </div>
+            <div class="mb-2">
+                <FormKit name="password" :config="{ classes: {input: 'border-gray-300 p-1 rounded shadow-sm border'}}"
+                    label="Mot de passe" type="password" />
+                <formKit label="Nouvel utilisateur ?"
+                    :submit-attrs="{ classes: {input: 'mt-4 bg-violet-300 border-violet-500 hover:bg-violet-500 rounded p-2'}}"
+                    name="nvlUtilisateur" type="checkbox" v-model="nvlUtilisateur" />
+            </div>
         </FormKit>
     </div>
 </template>
