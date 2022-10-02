b2ec6ed7e53db21f069dcad3062e85796979b883
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-10-02 18:52:17 +0200

    script formulaire maison

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -1,7 +1,38 @@
 <script setup lang="ts">
 import { ref } from "@vue/reactivity";
 import AfficheMaison from "./AfficheMaison.vue";
+import { supabase } from "../supabase";
 const dataMaison = ref({});
+
+// On fait une variable réactive qui réference les données
+// ATTENTION : faire une Ref pas une Reactive car :
+// c'est l'objet qui doit être réactif, pas ses props
+
+
+import { useRouter } from "vue-router";
+const router = useRouter();
+const props = defineProps(["id"]);
+if (props.id) {
+    // On charge les données de la maison
+    let { data, error } = await supabase
+        .from("maison")
+        .select("*")
+        .eq("id", props.id);
+    if (error) console.log("n'a pas pu charger le table Maison :", error);
+    else dataMaison.value = (data as any[])[0];
+}
+async function upsertMaison(dataForm, node) {
+    const { data, error } = await supabase.from("maison").upsert(dataForm);
+    if (error) node.setErrors([error.message]);
+    else {
+        node.setErrors([]);
+        router.push({ name: "edit-id", params: { id: data[0].id } });
+    }
+
+
+}
+
+
 </script>
 
 <template>
@@ -12,7 +43,8 @@ const dataMaison = ref({});
         </div>
         <div class="p-2">
             <FormKit type="form" :submit-attrs="{ classes: {input: 'bg-indigo-100 hover:bg-indigo-200 rounded p-2'}}"
-                :config="{ classes: {input: 'border-gray-300 p-1 rounded shadow-sm border'}}" v-model="dataMaison">
+                :config="{ classes: {input: 'border-gray-300 p-1 rounded shadow-sm border'}}" v-model="dataMaison"
+                @submit="upsertMaison">
                 <div class="mb-2">
                     <FormKit name="titre" label="Nom de la maison" />
                 </div>
@@ -30,3 +62,4 @@ const dataMaison = ref({});
         </div>
     </div>
 </template>
+
