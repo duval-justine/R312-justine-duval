4e02ce5b93a1834548e9afa3ba7dc07daa087d5e
duval-justine <95071327+duval-justine@users.noreply.github.com>
2022-10-02 18:51:57 +0200

    Contenue id.vue

---
+++ src/pages/edit/[id].vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import FormulaireOffreMaison from "../../components/FormulaireOffreMaison.vue"
+defineProps(["id"]);
+</script>
+            
+<template>
+    <div class="p-2">
+        <h2 class="text-3xl font-bold text-indigo-1000 text-center pt-[3.625rem] mb-8">Formulaire pour créer une offre
+        </h2>
+        <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(343px,1fr))]">
+            <FormulaireOffreMaison />
+        </div>
+    </div>
+</template>
